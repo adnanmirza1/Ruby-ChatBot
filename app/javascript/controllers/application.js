44d36508a3446f83253fbdc913f7f9db0530ae02
Adnan Mustafa <mirzaadnanmustafa@yahoo.com>
2023-08-01 21:51:07 +0500

    move rails ujs to controller/application.js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -5,5 +5,7 @@ const application = Application.start()
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+import Rails from '@rails/ujs';
 
+Rails.start();
 export { application }
